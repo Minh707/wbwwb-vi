0af7126bc67ed12a447cdf34600c3ea00d0c4547
Minh707 <us.66771508@gmail.com>
2023-06-29 20:08:44 +0700

    upload code

---
+++ js/textStrings.js
@@ -0,0 +1,108 @@
+///////////////////////////////////
+///// TEXT STRINGS ////////////////
+///////////////////////////////////
+
+var textStrings_EN = {
+	// Act 1
+	"niceHat": "OOH Mũ Đẹp",
+	"outtaHere": "yeah git Ra khỏi đây",
+	"getARoom": "GROSS, ĐI VỀ PHÒNG",
+	"notCoolAnymore": "mũ nvm không còn Trend nữa",
+	"tvOnTv": "TV... TRONG TV!",
+	"cricky": "LIL' CRICKY <3",
+	"tooManyCrickets": "Okay đó là rất nhiều dế",
+	"normalPeep": "Chỉ là một cái nhìn bình thường",
+	"normalPeeps": "Chỉ là một số cái nhìn bình thường",
+	"wowNothing": "WOWWEE, CHẲNG CÓ GÌ!",
+
+	// Act 2
+	"crazySquareAttacks": "HÌNH VUÔNG ĐANG ĐIÊN LOẠN",
+	"justMissed": "oooooh vừa bỏ lỡ nó",
+	"somethingInteresting": "(bạn phải chụp họ được khi họ *làm gì đó* thú vị...)",
+	"whoIsScreaming": "(Bạn phải chụp người đang hét vào mặt họ)",
+	"circleFearsSquares": "HÌNH TRÒN SỢ HÌNH VUÔNG",
+	"whoScaresThem": "(bạn phải chụp được *ai* đang làm họ sợ)",
+	"areTheyScared": "(bạn phải chụp được họ *đang* sợ hãi trước một hình vuông)", // not used
+	"squaresSnubCircles": "Hình Vuông SNUB Hình Tròn",
+	"areTheySnubbed": "(Bạn phải chụp họ *Trong khi* rình mò một hình tròn)", // not used
+	"everyoneHates": "MỌI NGƯỜI GHÉT NHAU!!!!",
+	"almostEveryoneHates": "HẦU HẾT MỌI NGƯỜI GHÉT NHAU...",
+	"squaresHateCircles": "HÌNH VUÔNG GHÉT HÌNH TRÒN",
+	"circlesHateSquares": "HÌNH TRÒN GHÉT HÌNH VUÔNG",
+	"areTheyYelling": "(bạn phải chụp họ *la hét* vào người khác)", // not used
+	"nerdsNow": "Bây giờ những mọt sách đang làm gì?",
+	"schockedPeep": "Tại sao peep này lại sốc?",
+	"whatever": "Sao cũng được",
+
+	// Act 3
+	"ellipsis": ". . .",
+	"coolNoMore": "Mũ không còn Trend nữa, bạn biết điều đó chưa?",
+	"beScared": "HÃY SỢ. HÃY GIẬN DỮ.",
+
+	// MANIFESTO
+	"manifesto": [
+		//"as if you viewers want GOOD news"       ,
+		"bao nhiêu người sẽ xem *mọi người tốt với nhau?*",
+		"hòa bình thì chán. bạo lực mới viral.",
+		//"peace is boring. conflict gets clicks." ,
+		"và mọi câu chuyện đầu cần có xung đột, vì vậy...",
+		//"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+		"CHO KHÁN GIẢ NHỮNG GÌ HỌ MUỐN."
+	],
+
+	// -- Strings currently in graphics resources --
+
+	// (bg_preload.png) Preload screen
+	"playingTime": "thời gian chơi: 5 phút",
+	"warning": "warning: cảnh báo: chương trình sau\nchứa những cảnh hợm hĩnh,\nthô lỗ & giết người hàng loạt.\nngười chơi nên thận trọng.",
+
+	// (cam-instructions.png) Camera Instructions
+	"pointAndClick": "ĐIỂM & CLICK",
+
+	// (chyron3.png)
+	"chyronNothing": "WOWWEE, NÓ *CHẲNG CÓ GÌ*",
+
+	// (credits0001.png)
+	"createdBy": "tạo bởi",
+	"NickyCase": "Nicky Case",
+
+	// (credits0002.png)
+	"manyThanks": "rất cảm ơn những người chơi thử của tôi:",
+
+	// (credits000[3,4,5,6].png)
+	"patreonSupporters": "và những người ủng hộ patreon của tôi:",
+
+	// (credits0007.png)
+	"lastButNotLeast": "và cuối cùng nhưng không kém phần quan trọng,",
+
+	// (credits0008.png)
+	"thankYouForPlaying": "cảm ơn BẠN vì đã chơi!",
+
+	// (preload_play.png)
+	"playButton": "PLAY",
+
+	// (end_button.png) Post Credits
+	"otherWorkButton": "Xem công việc khác của tôi",
+	"buyCoffeeButton": "Mua cho tôi một ly cà phê",
+	"replayButton": "chơi lại mớ hỗn độn này",
+
+	// (logo.png) Post Credits
+	"logoWBWWB": "CHIA SẺ NỖI ĐAU:",
+
+	// (end_prototype.png)
+	"endOfPrototype": "KẾT THÚC KIỂU NGUYÊN NHÂN",
+	"toBeContinued": "(to be continued!)",
+
+	// (quote0002.png)
+	"WBWWB": "Chúng ta trở thành những gì chúng ta thấy.",
+	"WSOTATOTSU": "Chúng tôi định hình các công cụ của chúng tôi và sau đó các công cụ của chúng tôi định hình chúng tôi.",
+
+	// (quote0003.png)
+	"MarshallMcLuhan": "Marshall McLuhan",
+
+	// (quote0004.png)
+	"misatrributed": "(phân bổ sai)",
+};
+
+// Umlaut codes: Ä = \xC4, Ö = \xD6, Ü = \xDC, ä = \xE4, ö = \xF6, ü = \xFC, ß = \x
+var textStrings = textStrings_EN
